dc4138a0697daaf20707e8c17191b6097bc6bd92
Thomas <thomasfaller@gmail.com>
2018-12-19 11:08:17 +0000

    re-made the repo

---
+++ front/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
+const app = express();
+const path = require('path');
+var content = require('./content/content');
+
+const PORT = 5000;
+
+app.set('view engine', 'pug');
+
+// SASS
+app.use(sassMiddleware({
+  // Options
+  src: __dirname,
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+
+// ROUTING
+app.get('/', (req, res) => {
+  res.render('index', content.index);
+});
+
+app.get('/docs', (req, res) => {
+  res.render('docs', content.docs)
+});
+app.get('/about', (req, res) => {
+  res.render('about', content.about)
+});
+
+app.listen(PORT, () => {
+  console.log(`Your app is running on https:localhost:${PORT}`);
+})
---
+++ front/content/content.js
@@ -0,0 +1,24 @@
+const index = {
+  active: 'Generator',
+  title: 'VeganIpsum API',
+  message: 'Welcome to VeganIpsum',
+  subtitle: 'A Lorem Ipsum generator that\'s 100% animal-cruelty free 🐄🐓🐑🐟\n❤'
+};
+
+const docs = {
+  active: 'API',
+  title: 'VeganIpsum - Docs',
+  message: 'Documentation',
+  subtitle: 'in progress...'
+};
+
+const about = {
+  active: 'About',
+  title: 'VeganIpsum - About',
+  message: 'About',
+  subtitle: 'I always thought there was a missing ipsum out there...'
+};
+
+module.exports.index = index;
+module.exports.docs = docs;
+module.exports.about = about;
